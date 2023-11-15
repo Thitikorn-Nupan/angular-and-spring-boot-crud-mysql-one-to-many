bb16dcce3103aae53386807f2aff1053d9cf6b19
ttknpde-v <thitikorn_nupan@outlook.co.th>
2023-11-15 15:23:17 +0700

    commit

---
+++ src/main/java/com/ttknpdev/understandhowtoworkwithangular/UnderstandHowToWorkWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.ttknpdev.understandhowtoworkwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandHowToWorkWithAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderstandHowToWorkWithAngularApplication.class, args);
+    }
+
+}
